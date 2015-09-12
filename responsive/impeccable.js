0253e6e1181d8648faa8ecfe960dc6588f084246
Sabhareesh Ravichandran <sabhareeshr@outlook.com>
2015-09-12 19:55:16 +0530

    Integrated with bootstrap; IE Check; BG Color transition

--- responsive/impeccable.js
+++ responsive/impeccable.js
@@ -1,5 +1,5 @@
 today = new Date();
-var BigDay = new Date("September 12, 2015 10:00:00");
+var BigDay = new Date("September 14, 2015 07:00:00");
 
 var countDownStartDate = new Date(BigDay);
 countDownStartDate.setDate(countDownStartDate.getDate() - 100);
@@ -10,38 +10,62 @@ var elapseInSecs = -(Math.round(elapseMilliSecs / 1000.0));
 
 jQuery(document).ready(function($) {
 
-	$('.time-part.hundredths.ones').css('-webkit-animation-timing-function', 'cubic-bezier(1.000, -0.530, 0.405, 1.425)');
-	$('.time-part.hundredths.ones').css('animation-timing-function', 'cubic-bezier(1.000, -0.530, 0.405, 1.425)');
+	var ua = window.navigator.userAgent;
+	var msie = ua.indexOf("MSIE ");
 
-	$('.time-part.hundredths.tens').css('-webkit-animation-timing-function', 'cubic-bezier(1.000, -0.530, 0.405, 1.425)');
-	$('.time-part.hundredths.tens').css('animation-timing-function', 'cubic-bezier(1.000, -0.530, 0.405, 1.425)');
+	var isIE = false;
 
-	$('.time-part.seconds.ones').css('-webkit-animation-timing-function', 'cubic-bezier(1.000, -0.530, 0.405, 1.425)');
-	$('.time-part.seconds.ones').css('animation-timing-function', 'cubic-bezier(1.000, -0.530, 0.405, 1.425)');
+	if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./))
+	{   
+		$(".time-part-wrapper").remove();
+		$('#sign-in-IE').hide();
+		isIE = true;
+	}
+	else
+	{ 
+		$('#sign-in').hide();
+		isIE = false;
+	}
 
-	$('.time-part.seconds.tens').css('-webkit-animation-timing-function', 'cubic-bezier(1,0,1,0)');
-	$('.time-part.seconds.tens').css('animation-timing-function', 'cubic-bezier(1,0,1,0)');
+	if( !isIE)
+	{
 
-	$('.time-part.minutes.ones').css('-webkit-animation-timing-function', 'cubic-bezier(1,0,1,0)');
-	$('.time-part.minutes.ones').css('animation-timing-function', 'cubic-bezier(1,0,1,0)');
+		$('.time-part.hundredths.ones').css('-webkit-animation-timing-function', 'cubic-bezier(1.000, -0.530, 0.405, 1.425)');
+		$('.time-part.hundredths.ones').css('animation-timing-function', 'cubic-bezier(1.000, -0.530, 0.405, 1.425)');
 
-	$('.time-part.minutes.tens .digit-wrapper').css('-webkit-animation-timing-function', 'step-end');
-	$('.time-part.minutes.tens .digit-wrapper').css('animation-timing-function', 'step-end');
+		$('.time-part.hundredths.tens').css('-webkit-animation-timing-function', 'cubic-bezier(1.000, -0.530, 0.405, 1.425)');
+		$('.time-part.hundredths.tens').css('animation-timing-function', 'cubic-bezier(1.000, -0.530, 0.405, 1.425)');
 
-	$('.time-part.hours.ones .digit-wrapper').css('-webkit-animation-timing-function', 'step-end');
-	$('.time-part.hours.ones .digit-wrapper').css('animation-timing-function', 'step-end');
+		$('.time-part.seconds.ones').css('-webkit-animation-timing-function', 'cubic-bezier(1.000, -0.530, 0.405, 1.425)');
+		$('.time-part.seconds.ones').css('animation-timing-function', 'cubic-bezier(1.000, -0.530, 0.405, 1.425)');
 
-	$('.time-part.hours.tens .digit-wrapper').css('-webkit-animation-timing-function', 'step-end');
-	$('.time-part.hours.tens .digit-wrapper').css('animation-timing-function', 'step-end');
+		$('.time-part.seconds.tens').css('-webkit-animation-timing-function', 'cubic-bezier(1,0,1,0)');
+		$('.time-part.seconds.tens').css('animation-timing-function', 'cubic-bezier(1,0,1,0)');
 
-	$('.time-part.days.ones .digit-wrapper').css('-webkit-animation-timing-function', 'step-end');
-	$('.time-part.days.ones .digit-wrapper').css('animation-timing-function', 'step-end');
+		$('.time-part.minutes.ones').css('-webkit-animation-timing-function', 'cubic-bezier(1,0,1,0)');
+		$('.time-part.minutes.ones').css('animation-timing-function', 'cubic-bezier(1,0,1,0)');
 
-	$('.time-part.days.tens .digit-wrapper').css('-webkit-animation-timing-function', 'step-end');
-	$('.time-part.days.tens .digit-wrapper').css('animation-timing-function', 'step-end');
+		$('.time-part.minutes.tens .digit-wrapper').css('-webkit-animation-timing-function', 'step-end');
+		$('.time-part.minutes.tens .digit-wrapper').css('animation-timing-function', 'step-end');
 
-	$('.digit-wrapper').css('-webkit-animation-delay', elapseInSecs + 's');
-	$('.digit-wrapper').css('animation-delay', elapseInSecs + 's');
+		$('.time-part.hours.ones .digit-wrapper').css('-webkit-animation-timing-function', 'step-end');
+		$('.time-part.hours.ones .digit-wrapper').css('animation-timing-function', 'step-end');
 
-	$('.digit-wrapper').addClass('animation-running');
+		$('.time-part.hours.tens .digit-wrapper').css('-webkit-animation-timing-function', 'step-end');
+		$('.time-part.hours.tens .digit-wrapper').css('animation-timing-function', 'step-end');
+
+		$('.time-part.days.ones .digit-wrapper').css('-webkit-animation-timing-function', 'step-end');
+		$('.time-part.days.ones .digit-wrapper').css('animation-timing-function', 'step-end');
+
+		$('.time-part.days.tens .digit-wrapper').css('-webkit-animation-timing-function', 'step-end');
+		$('.time-part.days.tens .digit-wrapper').css('animation-timing-function', 'step-end');
+
+		$('.digit-wrapper').css('-webkit-animation-delay', elapseInSecs + 's');
+		$('.digit-wrapper').css('-o-animation-delay', elapseInSecs + 's');
+		$('.digit-wrapper').css('-moz-animation-delay', elapseInSecs + 's');
+		$('.digit-wrapper').css('animation-delay', elapseInSecs + 's');
+
+		$('.digit-wrapper').addClass('animation-running');
+
+	}
 });
