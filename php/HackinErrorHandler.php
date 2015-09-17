7a09b36c7dc24d1717be8737cc3f3099f0b78063
ThirukkaKarnan <thirukkakarnan@gmail.com>
2015-09-17 18:06:12 +0530

    Minor change in error handler, to support general exceptions. Yet to include an error model and page

--- php/HackinErrorHandler.php
+++ php/HackinErrorHandler.php
@@ -1,6 +1,7 @@
 <?php
     /**
         Global, centralized error handler for Hackin event
+        TODO: Create error model to be help handling easier in frontend.
     */
     class HackinErrorHandler {
         /**
@@ -8,7 +9,7 @@
         */
         public static function errorHandler(Exception $ex, $errorOccuredIn) {
             $errorMsg = "FATAL!! error occured during **".$errorOccuredIn."** <br>Error Msg=".$ex->getMessage();
-            $errorMsg = $errorMsg . self::errorMsgToShowToPlayer();
+            $errorMsg = $errorMsg . self::errorMsgToShowToHackinUser();
             //log in the file here
             return $errorMsg;
         }
@@ -17,12 +18,14 @@
             Use this function to integrate with the error page
             TODO: String output based on the error
         */
-        private static function errorMsgToShowToPlayer() {
-            $errorMsg = "<br> Quiting process.. Refresh after sometime or kindly contact event co-ordinator.".
+        private static function errorMsgToShowToHackinUser() {
+            $errorMsg = "<br><br> Errors are thrown only when invalid and unauthorized requests are made.".
+                         "<br> Better clear cookies and Login again from the <a href=\"http://hackin.psglogin.in\">hackin event home</a>". 
+                         "<br> If the problem persists, kindly contact the event co-ordinator.".
                          "<br> For details of the co-ordinator, refer ".
                          "<a href=\"http://psglogin.in/hackin.php\">hackin</a> event page under ".
                          "<a href=\"http://psglogin.in\">Login15</a> <br>";
             return $errorMsg;
         }
-}
+    }
 ?>
